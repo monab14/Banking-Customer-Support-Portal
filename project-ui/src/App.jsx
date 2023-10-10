9ab48b0298db2fb0529c69fdd109b50a91a09f04
Tabassumjahan <khansimrantabassum@gmail.com>
2023-10-10 15:27:31 +0530

    changes done

---
+++ project-ui/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          hello hello ji 
+        </h1>
+        <a>
+          Welcome
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
