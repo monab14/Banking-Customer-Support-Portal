60ba291c3b7d9d3283b19593e09f459717e96afc
Monali Gulab Borade <110600441+monab14@users.noreply.github.com>
2023-10-12 05:25:00 +0000

    c

---
+++ project-ui/src/components/BannerCarousel.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+
+const BannerCarousel = () => {
+    return (
+    <div className="col text-center container mb-3">
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="1.jpg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="2.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+    <img
+          className="d-block w-100"
+          src="3.jpg"
+          alt="Third slide"
+        />
+    </Carousel.Item>
+    <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="4.jpg"
+          alt="fourth slide"
+        />
+          </Carousel.Item>
+          <Carousel.Item>
+        <img
+          className="d-block h-90 w-100"
+          src="5.jpg"
+          alt="fifth slide" 
+        />
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  );
+};
+
+export default BannerCarousel;
