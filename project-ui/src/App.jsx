86989a7a9817f149eb46b33101f5820935d1b8e8
radhikachandel <71997441+radhikachandel@users.noreply.github.com>
2023-10-14 09:23:47 +0000

    customer Dashboard

--- project-ui/src/App.jsx
+++ project-ui/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+<<<<<<< HEAD
 import {  Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import ThankYouPage from './pages/customer/ThankYouPage';
+=======
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import CustomerDashboard from './pages/CustomerDashboard';
+>>>>>>> b5be628 (customer Dashboard)
 import LoginPage from './pages/customer/LoginPage';
 import AdminLoginPage from './pages/Admin/Adminlogin';
 import RegistrationPage from './pages/customer/RegistrationPage';
@@ -11,27 +18,38 @@ import AdminAddFaq from './pages/Admin/AdminAddFaq';
 import CustomerSupportPage from './pages/customer/CustomerSupportPage';
 
 function App() {
+<<<<<<< HEAD
   const navigate = useNavigate();
   return (
     <div className="App">
       
-      
-        <Routes>
+=======
+  const navigate = useNavigate(); // Get the navigate function
 
+  return (
+    <div className="App">
+      {/* Conditionally render the NavBar */}
+      {window.location.pathname !== '/customer-dashboard' && <NavBar/>}
+>>>>>>> b5be628 (customer Dashboard)
+      
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/Adminlogin" element={<AdminLoginPage />} />
         {/* <Route path="/adminAddFaq" element={<AdminAddFaq />} /> */}
         <Route path="/RegistrationPage" element={<RegistrationPage />} />
+<<<<<<< HEAD
         <Route path="/thank-you" element={<ThankYouPage navigate={navigate} />} />
         <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/Support" element={<CustomerSupportPage />} />
         </Routes>
      
+=======
+        <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+      </Routes>
+>>>>>>> b5be628 (customer Dashboard)
     </div>
   );
 }
 
-
 export default App;
-
---
+++ project-ui/src/pages/DashboardCarousel.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const DashboardCarousel = () => {
+    return (
+      <div className="col-md-6 text-center mb-1 border border-secondary">
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="6.png"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="9.jpg_large"
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="8.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="4.jpg"
+                        alt="fourth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="5.jpg"
+                        alt="fifth slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default DashboardCarousel;
