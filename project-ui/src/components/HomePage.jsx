7b48412ff0ee610fbf0494cd3c57cb59d8abf0f5
Monali Gulab Borade <110600441+monab14@users.noreply.github.com>
2023-10-17 06:43:29 +0000

    c

--- project-ui/src/components/HomePage.jsx
+++ project-ui/src/components/HomePage.jsx
@@ -14,14 +14,14 @@ const HomePage = () => {
     return (
         <div className="HomePage">
             <NavBar/>
-            <div className=" container mb-5 mt-5 rounded" style={containerStyle}>
-                <h1 className="text-center" style={{ color: '#750D37' }}>Welcome to Axis bank</h1>
+            <div className=" container " style={{textAlign:'center'}}>
+            <h1 style={{ color: '#750D37' }}><strong>Welcome to Axis Bank.</strong></h1>
+               
             </div>
             <img src='MonaImages/dil se open.jpg' width='1520px' height='500px'></img>
             <br/> <br/><br/>
             
             <img src='MonaImages/home.png' width='1520px' height='500px' style={imgStyle}></img>
-            <br/>
             <BannerCarousel/>
             <FaqSection />
             <img src='MonaImages/home2.png' width='1520px' height='500px'  ></img>
