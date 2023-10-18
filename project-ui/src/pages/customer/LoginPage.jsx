1eaa9e861bf1cf422cb92db9d939f538dff45d2c
Tabassumjahan <khansimrantabassum@gmail.com>
2023-10-18 15:33:38 +0530

    login page

--- project-ui/src/pages/customer/LoginPage.jsx
+++ project-ui/src/pages/customer/LoginPage.jsx
@@ -1,83 +1,53 @@
-
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import login from "../../images/login.png";
 import NavBar from '../../components/NavBar';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+const parentContainerStyle = {
+  display: 'flex',
+  width: '100vw',
+  height: '100vh',
+};
 
+const imageContainerStyle = {
+  flex: '1',
+  display: 'flex',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  padding: '40px',
+  borderRadius: '10px',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
-  const imageStyle = {
-    width: '50%',
-    height: '90vh',
-    objectFit: 'cover',
-    padding: '20px',
+const imageStyle = {
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+};
 
-  };
-
-  const containerStyle = {
-    width: '50%',
-    padding: '120px',
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F0F2EF',
-  };
-
-  const innerContainerStyle = {
-    width: '100%',
-    padding: '50px',
-    borderRadius: '10px',
-    backgroundColor: '#FFFFFF',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  };
-
-  const inputStyle = {
-    margin: '10px 0',
-    padding: '10px',
-    width: '100%',
-    fontSize: '16px',
-    color: '#871f40',
-  };
-
-  const buttonStyle = {
-    margin: '20px 0',
-    padding: '10px 20px',
-    fontSize: '18px',
-    cursor: 'pointer',
-    backgroundColor: '#871f40',
-    color: '#F0F2EF',
-    borderRadius: '20px',
-    border: 'none',
-    display: 'flex',
-    alignItems: 'center',
-  };
-
-  const iconStyle = {
-    marginRight: '10px',
-    color: '#FFFFFF',
-  };
-
-  const logoStyle = {
-    position: 'absolute',
-    top: '10px',
-    right: '10px',
-  };
+const formContainerStyle = {
+  flex: '1',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  
+  flexDirection: 'column',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  padding: '70px',
+  borderRadius: '10px',
+  backgroundColor: '#FFFFFF',
+   
+};
 
 const LoginPage = () => {
-  
-const navigate = useNavigate();
-
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-   username: '',
+    email: '',
     password: '',
   });
- const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -87,9 +57,8 @@ const navigate = useNavigate();
     e.preventDefault();
     try {
       const response = await axios.get('http://localhost:8090/api/welcome', formData);
-      console.log(response.data); 
+      console.log(response.data);
       navigate('/customer-dashboard');
-
     } catch (error) {
       console.error('Error occurred while logging in:', error);
       setError('Invalid username or password. Please try again.');
@@ -97,100 +66,55 @@ const navigate = useNavigate();
   };
 
   return (
-    <div><NavBar />
-      <div className="login-page" style={{ display: 'flex', width: '100vw', height: '100vh' }}>
-        <div className=" mt-5" style={{ ...imageStyle }}>
+    <div>
+      <NavBar />
+      <div className="login-page" style={parentContainerStyle}>
+        <div className="image-container" style={imageContainerStyle}>
           <img
             src="https://pbs.twimg.com/media/DlStQXeUUAEhcpC?format=jpg&name=small"
             alt="Bank"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            style={imageStyle}
           />
-        </div> 
-        <div className="inner mt-5 " style={{ ...innerContainerStyle }}>
-        <a className="navbar-brand mb-3" href="#">
+        </div>
+        <div className="form-container" style={formContainerStyle}>
+          {error && <div style={{ color: 'red', textAlign: 'center', marginTop: '10px' }}>{error}</div>}
+          <div className="mt-4" style={{ ...formContainerStyle }}>
             <img src={login} alt="Login" width="150px" />
-          </a>
-        <h2 style={{ color: '#750D37', textAlign: 'left' }}>Customer Login</h2>
-        <h6 style = {{ color : '#750D37'}}> Sign in to continue to the website </h6>
-        <form style={{ width: '100%' }} onSubmit={handleSubmit}>
-          <div className="login-input-container mt-3 mb-3" style={{ ...inputStyle }}>
-            <label className= "form-label">Username : </label>
-                <input
-                type="text"
-                id="username"
-                name="username"
-                className="form-control"
-                required
-            />
-          </div>
-          <div className="login-input-container mb-3" style={{ ...inputStyle }}>
-            <label className="form-label">Password : </label>
-            <input
-                type="password"
-                id="password"
-                className="form-control"
-                placeholder= "*********"
-                required
-            />
-        </div>
-        <div className="forgot-password mb-3" style={{ textAlign: 'right', width: '100%' }}>
-            <a href="#">Forgot Password ?</a>
-        </div>
-        <div className="mt-3" style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-          <button type="submit" style={{ ...buttonStyle }}>
-            <span style={iconStyle}>🔒</span> Login
-            </button>
-        </div>
-        </form>
-        {error && <div style={{ color: 'red', textAlign: 'center', marginTop: '10px' }}>{error}</div>}
-
-        </div>
-        <div className="Container" style={{ ...containerStyle }}>
-          <div className=" mt-4" style={{ ...logoStyle }}>
-            <img
-              src="https://bv.world/wp-content/uploads/2023/01/AxisBank-logo.jpg"
-              alt="Logo"
-              style={{ width: '180px', height: '50px' }}
-            />
-          </div>
-          <div className="inner mt-5 " style={{ ...innerContainerStyle }}>
-            <a className="navbar-brand mb-3" href="#">
-              <img src={login} alt="Login" width="150px" />
-            </a>
             <h2 style={{ color: '#871f40', textAlign: 'left' }}>Customer Login</h2>
-            <h6 style={{ color: '#871f40' }}> Sign in to continue to the website </h6>
-            <form style={{ width: '100%' }}>
-              <div className="login-input-container mt-3 mb-3" style={{ ...inputStyle }}>
-                <label className="form-label">Username : </label>
+            <h6 style={{ color: '#871f40' }}>Sign in to continue to the website</h6>
+            <form style={{ width: '100%' }} onSubmit={handleSubmit}>
+              <div className="login-input-container mt-3 mb-3">
+                <label className="form-label">Username:</label>
                 <input
                   type="email"
                   id="email"
                   name="email"
                   className="form-control"
                   required
+                  onChange={handleChange}
                 />
               </div>
-              <div className="login-input-container mb-3" style={{ ...inputStyle }}>
-                <label className="form-label">Password : </label>
+              <div className="login-input-container mb-3">
+                <label className="form-label">Password:</label>
                 <input
                   type="password"
                   id="password"
                   className="form-control"
                   placeholder="*********"
                   required
+                  onChange={handleChange}
                 />
               </div>
               <div className="forgot-password mb-3" style={{ textAlign: 'right', width: '100%' }}>
-                <a href="#">Forgot Password ?</a>
+                <a href="#">Forgot Password?</a>
               </div>
-              <div className="mt-3" style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-                <button type="submit" style={{ ...buttonStyle }}>
-                  <span style={iconStyle}>🔒</span> Login
+              <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                <button type="submit" style={{ margin: '20px 0', padding: '10px 20px', fontSize: '18px', cursor: 'pointer', backgroundColor: '#871f40', color: '#F0F2EF', borderRadius: '20px', border: 'none' }}>
+                  <span style={{ marginRight: '10px', color: '#FFFFFF' }}>🔒</span> Login
                 </button>
               </div>
             </form>
           </div>
-
         </div>
       </div>
     </div>
